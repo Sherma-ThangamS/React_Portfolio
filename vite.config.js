8f932fd91b2780f343e156023044ad6454297fb9
Sherma Thangam S <119473142+Sherma-ThangamS@users.noreply.github.com>
2023-09-19 10:38:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/React_Portfolio",
+  base : "/React_Portfolio/",
   css: {
     modules: {
       localsConvention: "camelCase",
