60603669e686e3d96c0e6600270d9491b6573102
Sherma Thangam S <119473142+Sherma-ThangamS@users.noreply.github.com>
2024-02-24 22:56:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -78,30 +78,6 @@ function App() {
   }
   useEffect(() => {
     animateCircles();
-    // const circles = circlesRef.current;
-
-    // let x = coords.x;
-    // let y = coords.y;
-
-    // circles.forEach((circle, index) => {
-    //   circle.style.left = x - 12 + 'px';
-    //   circle.style.top = y - 12 + 'px';
-
-    //   circle.style.transform = `scale(${(circles.length - index) / circles.length})`;
-
-    //   circle.x = x;
-    //   circle.y = y;
-
-    //   const nextCircle = circles[index + 1] || circles[0];
-    //   x += (nextCircle.x - x) * 0.3;
-    //   y += (nextCircle.y - y) * 0.3;
-    // });
-    // requestAnimationFrame(() => {
-    //   circles.forEach((circle) => {
-    //     circle.style.left = x - 12 + 'px';
-    //     circle.style.top = y - 12 + 'px';
-    //   });
-    // });
   }, [coords]);
   
   return (
