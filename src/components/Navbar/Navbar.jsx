0f563301141e23ece358bc0de99d503c0e7e30c6
Sherma Thangam S <119473142+Sherma-ThangamS@users.noreply.github.com>
2024-02-25 12:06:52 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">
+      <Link className={styles.title} href="/">
         Portfolio
-      </a>
+      </Link>
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
